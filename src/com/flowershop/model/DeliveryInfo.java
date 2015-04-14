d4025df646668804f67bd02379924d3c80a13575
Jeffrey Angell <jeffreyangell@byu.net>
2015-04-14 12:40:24 -0600

    Attempted send driver a message to accept order

--- src/com/flowershop/model/DeliveryInfo.java
+++ src/com/flowershop/model/DeliveryInfo.java
@@ -4,6 +4,7 @@ package com.flowershop.model;
  * Created by jeffrey on 4/14/15.
  */
 public class DeliveryInfo {
+    private int id;
     private Location shopLocation;
     private int orderId;
 
@@ -21,5 +22,13 @@ public class DeliveryInfo {
 
     public void setOrderId(int orderId) {
         this.orderId = orderId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
